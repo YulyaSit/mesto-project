d4e6183b6ad1e582efa0357497464522642d00c4
Vitaliy Yarash <vit.iarosch@yandex.by>
2023-06-03 07:33:27 +0300

    fix: класс userinfo

---
+++ src/components/UserInfoo.js
@@ -0,0 +1,24 @@
+class UserInfo {
+    // теперь все данные пользователя устанавливаются тут, включая аватар
+    constructor({ nameSelector, jobSelector, avatarSelector }) {
+      this._nameElement = document.querySelector(nameSelector);
+      this.jobElement = document.querySelector(jobSelector);
+      this._avatarElement = document.querySelector(avatarSelector);
+    }
+
+    // метод getUserInfo должен возвращать данные из профиля, взяв их из `textContent`.
+    getUserInfo = () => {
+      return {
+        name: this._nameElement.textContent,
+        about: this.jobElement.textContent,
+        userId: this._userId,  // _id тоже можно тут получать, чтобы испольозовать в `index.js` для создания карточек
+      };
+    };
+    // метод `setUserInfo` должен получать в вызов все данные пользователя и устанавливать их внутри
+    setUserInfo({ name, about, avatar, _id }) {
+      this._nameElement.textContent = name;
+      this.jobElement.textContent = about;
+      this._avatarElement.src = avatar;
+      this._userId = _id;
+    }
+  }
